07ec8d77748e5fe4913b675f40a77aa9374237d4
Fauzbyte <muhammadfauzanpratama420@gmail.com>
2026-08-02 20:19:10 +0700

    ready

--- utils/qr_code.go
+++ utils/qr_code.go
@@ -2,10 +2,10 @@ package utils
 
 import "github.com/skip2/go-qrcode"
 
-func GenerateQRCode(token string) ([]byte,error){
-    return qrcode.Encode(
-        token,
-        qrcode.Medium,
-        256,
-    )
+func GenerateQRCode(token string) ([]byte, error) {
+	return qrcode.Encode(
+		token,
+		qrcode.Highest, // Level H error correction (30% capacity)
+		512,            // 512x512 crisp high-res
+	)
 }
